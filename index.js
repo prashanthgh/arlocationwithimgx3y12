016618aaf04ec86263a7bf65f90a8e367079d68d
prashanthgh <34641309+prashanthgh@users.noreply.github.com>
2022-09-05 18:43:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,114 +1,54 @@
-function isMobile() {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        // true for mobile device
-        return true;
-    }
-    return false;
-}
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(80, 2, 0.1, 50000);
-const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#canvas1') });
-
-const geom = new THREE.BoxGeometry(20,20,20);
-
-const threex = new THREEx.LocationBased(scene, camera);
-
-// You can change the minimum GPS accuracy needed to register a position - by default 1000m
-//const threex = new THREEx.LocationBased(scene, camera. { gpsMinAccuracy: 30 } );
-const cam = new THREEx.WebcamRenderer(renderer, '#video1');
-
-const oneDegAsRad = THREE.Math.degToRad(1);
-
-
-let orientationControls;
-
-if (isMobile()){   
-    orientationControls = new THREEx.DeviceOrientationControls(camera);
-} 
-
-let fake = null;
-let first = true;
-
-threex.on("gpsupdate", pos => {
-    console.log('gpsupdate');
-    if(first) {
-        setupObjects(pos.coords.longitude, pos.coords.latitude);
-        first = false;
-    }
-});
-
-threex.on("gpserror", code => {
-    alert(`GPS error: code ${code}`);
-});
-
-// Uncomment to use a fake GPS location
-//fake = { lat: 51.05, lon : -0.72 };
-if(fake) {
-    threex.fakeGps(fake.lon, fake.lat);
-} else {
-    threex.startGps();
-} 
-
-requestAnimationFrame(render);
-
-
-
-let mousedown = false, lastX =0;
-
-// Mouse events for testing on desktop machine
-if(!isMobile()) {
-    window.addEventListener("mousedown", e=> {
-        mousedown = true;
-    });
-
-    window.addEventListener("mouseup", e=> {
-        mousedown = false;
-    });
-
-    window.addEventListener("mousemove", e=> {
-        if(!mousedown) return;
-        if(e.clientX < lastX) {
-            camera.rotation.y -= oneDegAsRad; 
-            if(camera.rotation.y < 0) {
-                camera.rotation.y += 2 * Math.PI;
+window.onload = () => {
+    let testEntitiesAdded = false;
+    alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
+    const el = document.querySelector("[gps-new-camera]");
+    el.addEventListener("gps-camera-update-position", e => {
+        if(!testEntitiesAdded) {
+            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+            // Add four boxes to the north (red), south (yellow), west (blue)
+            // and east (red) of the initial GPS position
+            const properties = [{
+                    color: 'red',
+                    latDis: 0.001,
+                    lonDis: 0
+                },{
+                    color: 'yellow',
+                    latDis: -0.001,
+                    lonDis: 0
+                },{
+                    color: 'blue',
+                    latDis: 0,
+                    lonDis: -0.001
+                },{
+                    color: 'green',
+                    latDis: 0,
+                    lonDis: 0.001
+                }
+            ];
+            for(const prop of properties) {
+                const entity = document.createElement("a-box");
+                entity.setAttribute("scale", {
+                    x: 20, 
+                    y: 20,
+                    z: 20
+                });
+                entity.setAttribute('material', { color: prop.color } );
+                entity.setAttribute('gps-new-entity-place', {
+                    latitude: e.detail.position.latitude + prop.latDis,
+                    longitude: e.detail.position.longitude + prop.lonDis
+                });
+                
+                document.querySelector("a-scene").appendChild(entity);
             }
-        } else if (e.clientX > lastX) {
-            camera.rotation.y += oneDegAsRad;
-            if(camera.rotation.y > 2 * Math.PI) {
-                camera.rotation.y -= 2 * Math.PI;
-            }
+            testEntitiesAdded = true;
         }
-        lastX = e.clientX;
     });
-}
 
-function render(time) {
-    resizeUpdate();
-    if(orientationControls) orientationControls.update();
-    cam.update();
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
-}
+    document.getElementById("go").addEventListener("click", e=> {
+        const lat = document.getElementById('lat').value;
+        const lon = document.getElementById('lon').value;
+        const minacc = document.getElementById('minacc').value;
 
-function resizeUpdate() {
-    const canvas = renderer.domElement;
-    const width = canvas.clientWidth, height = canvas.clientHeight;
-    if(width != canvas.width || height != canvas.height) {
-        renderer.setSize(width, height, false);
-    }
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
-}
-
-function setupObjects(longitude, latitude) {
-    // Use position of first GPS update (fake or real)
-    const material = new THREE.MeshBasicMaterial({color: 0xff0000});
-    const material2 = new THREE.MeshBasicMaterial({color: 0xffff00});
-    const material3 = new THREE.MeshBasicMaterial({color: 0x0000ff});
-    const material4 = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    threex.add(new THREE.Mesh(geom, material), longitude, latitude + 0.001); // slightly north
-    threex.add(new THREE.Mesh(geom, material2), longitude, latitude - 0.001); // slightly south
-    threex.add(new THREE.Mesh(geom, material3), longitude - 0.001, latitude); // slightly west
-    threex.add(new THREE.Mesh(geom, material4), longitude + 0.001, latitude); // slightly east
-}
+        el.setAttribute('gps-new-camera', { simulateLatitude: lat, simulateLongitude: lon, positionMinAccuracy: minacc } );
+    });
+};
